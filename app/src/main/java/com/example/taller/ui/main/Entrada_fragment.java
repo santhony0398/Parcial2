14d960bf9061dfa1ec2d26a556f758feec25b3e3
Anthony Santana <santhony0398@gmail.com>
2020-07-16 22:46:49 -0500

    Revert "Layout Entradas"
    This reverts commit 4ceadc3f096bb2661f273d22323446cdbb1b7c96.

--- app/src/main/java/com/example/taller/ui/main/Entrada_fragment.java
+++ app/src/main/java/com/example/taller/ui/main/Entrada_fragment.java
@@ -3,13 +3,10 @@ package com.example.taller.ui.main;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 
 import com.example.taller.R;
 
@@ -23,9 +20,6 @@ public class Entrada_fragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    private Spinner spinnerMes;
-    private Spinner spinnerAnyo;
-    private Spinner spinnerMostrarPor;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -65,21 +59,7 @@ public class Entrada_fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_entrada, container,false);
-        spinnerMes=v.findViewById(R.id.spinnerMes);
-        spinnerAnyo=v.findViewById(R.id.spinnerAnyo);
-        spinnerMostrarPor=v.findViewById(R.id.spinnerMostrarPor);
-        String[] meses={"Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"};
-        String[] anyos={"2018","2019","2020"};
-        String[] mostrarPor={"Fav1","Fav2","Fav3"};
-        ArrayAdapter<String> adapterMes = new ArrayAdapter<>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, meses);
-        spinnerMes.setAdapter(adapterMes);
-        ArrayAdapter <String> adapterAnyo = new ArrayAdapter<>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, anyos);
-        spinnerAnyo.setAdapter(adapterAnyo);
-        ArrayAdapter <String> adapterMPor = new ArrayAdapter<>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, mostrarPor);
-        spinnerMostrarPor.setAdapter(adapterMPor);
-
-        RecyclerView recyclerTarjetas = (RecyclerView) v.findViewById(R.id.lista_entradas);
-        return v;
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_entrada, container, false);
     }
 }
